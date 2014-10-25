aea5175e07e9815569aa9411ca40f847af6c571b
Ge Jian Feng <gejianfeng2@126.com>
2014-10-26 00:37:14 +0800

    + Add Example Code

--- CH02/Triangle/src/Triangle.cpp
+++ CH02/Triangle/src/Triangle.cpp
@@ -16,56 +16,58 @@
 #include <GL/glut.h>
 #endif
 
-GLBatch					triangleBatch;
-GLShaderManager			shaderManager;
+GLBatch             triangleBatch;
+GLShaderManager     shaderManager;
 
 void SetupRC()
 {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	shaderManager.InitializeStockShaders();
-
-	GLfloat vVerts[] = { -0.5f, 0.0f, 0.0f,
-						  0.5f, 0.0f, 0.0f,
-						  0.0f, 0.5f, 0.0f };
-
-	triangleBatch.Begin(GL_TRIANGLES, 3);
-	triangleBatch.CopyVertexData3f(vVerts);
-	triangleBatch.End();
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    shaderManager.InitializeStockShaders();
+    
+    GLfloat vVerts[] = { -0.5f, 0.0f, 0.0f,
+                          0.5f, 0.0f, 0.0f,
+                          0.0f, 0.5f, 0.0f };
+    
+    triangleBatch.Begin(GL_TRIANGLES, 3);
+    triangleBatch.CopyVertexData3f(vVerts);
+    triangleBatch.End();
 }
 
 void RenderScene()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-	GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-	shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
-	triangleBatch.Draw();
-	glutSwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    
+    GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
+    triangleBatch.Draw();
+    
+    glutSwapBuffers();
 }
 
 void ChangeSize(int width, int height)
 {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
 
 int main(int argc, char* argv[])
 {
-	gltSetWorkingDirectory(argv[0]);
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("Triangle");
-
-	glutReshapeFunc(ChangeSize);
-	glutDisplayFunc(RenderScene);
-
-	GLenum err = glewInit();
-	if (err != GLEW_OK) {
-		fprintf(stderr, "GLEW ERROR: %s\n", glewGetErrorString(err));
-		return 1;
-	}
-
-	SetupRC();
-	glutMainLoop();
-
-	return 0;
+    gltSetWorkingDirectory(argv[0]);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Triangle");
+    
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(RenderScene);
+    
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr, "GLEW ERROR: %s\n", glewGetErrorString(err));
+        return 1;
+    }
+    
+    SetupRC();
+    glutMainLoop();
+    
+    return 0;
 }
